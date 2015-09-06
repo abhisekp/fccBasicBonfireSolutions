9cfb1d626394166dc37afadb030a21e9a0bceb32
Abhisek Pattnaik <abhisekp@engineer.com>
2015-09-07 04:43:00 +0530

    Add tests

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+var abhisekpFCCbasicBonfireSolutions = require(
+        "../lib/fcc-basic-bonfire-solutions"),
+    apFCCbbfs = abhisekpFCCbasicBonfireSolutions;
+
+var chai = require("chai"),
+    should = chai.should,
+    expect = chai.expect,
+    assert = chai.assert;
+
+
+
+describe("Test Runs Successfully", function() {
+    it("should return true", function() {
+
+    });
+});
