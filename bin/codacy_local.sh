f4fef728e6499ea5d6b5eb44b0bdce6c68a8c382
Abhisek Pattnaik <abhisekp@engineer.com>
2015-09-10 05:19:52 +0530

    export variable

--- bin/codacy_local.sh
+++ bin/codacy_local.sh
@@ -1,4 +1,4 @@
 #! sh
 
-CODACY_PROJECT_TOKEN=WRITE_YOUR_SECRET_TOKEN_HERE
+export CODACY_PROJECT_TOKEN=WRITE_YOUR_SECRET_TOKEN_HERE
 NODE_ENV=test YOURPACKAGE_COVERAGE=1 node_modules/.bin/mocha --es_staging --harmony --recursive -r blanket -R mocha-lcov-reporter -C test | node_modules/.bin/codacy-coverage -v
