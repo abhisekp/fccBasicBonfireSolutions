fffaa0ef0e9987285d4e357ac5e771ba49f3fcd4
Abhisek Pattnaik <abhisekp@engineer.com>
2015-09-10 05:09:09 +0530

    Set service name as local
    - export variables

--- bin/coveralls_local.sh
+++ bin/coveralls_local.sh
@@ -1,4 +1,5 @@
 #! sh
 
-COVERALLS_REPO_TOKEN=WRITE_YOUR_SECRET_TOKEN_HERE
-NODE_ENV=test YOURPACKAGE_COVERAGE=1 mocha --es_staging --harmony --recursive -r blanket -R node_modules/.bin/mocha-lcov-reporter -C test | node_modules/.bin/coveralls
+export COVERALLS_SERVICE_NAME=local
+export COVERALLS_REPO_TOKEN=WRITE_YOUR_SECRET_TOKEN_HERE
+NODE_ENV=test YOURPACKAGE_COVERAGE=1 node_modules/.bin/mocha --es_staging --harmony --recursive -r blanket -R mocha-lcov-reporter -C test | node_modules/.bin/coveralls
